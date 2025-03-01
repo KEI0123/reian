23c77b2ce1db9928e837f6703050f992c906889f
KEI0123 <keishu.sgtm@gmail.com>
2025-03-01 23:09:56 +0900

    characterの内容を実装

--- js/download.js
+++ js/download.js
@@ -1,5 +1,5 @@
 function download(version, platform) {
     //const downloadLink = `path_to_downloads/version_${version}_${platform}bit.zip`;
-    const downloadLink = `assets/sample.zip`;
+    const downloadLink = `https://github.com/KEI0123/reian/raw/main/assets/sample.zip`;
     window.location.href = downloadLink;
 }
