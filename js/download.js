e543455a98b8a1fb873a8533c4b4e272d15e2b95
KEI0123 <keishu.sgtm@gmail.com>
2025-03-01 22:54:06 +0900

    downloadのリンクをjsに実装

---
+++ js/download.js
@@ -0,0 +1,5 @@
+function download(version, platform) {
+    //const downloadLink = `path_to_downloads/version_${version}_${platform}bit.zip`;
+    const downloadLink = `assets/sample.zip`;
+    window.location.href = downloadLink;
+}
