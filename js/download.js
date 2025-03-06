6ae05acbd2fda24c485ab06a9a2bff2c8bbfedb3
KEI0123 <keishu.sgtm@gmail.com>
2025-03-06 20:19:26 +0900

    Google Analyticsのコードをそれぞれのhtmlに追加

--- js/download.js
+++ js/download.js
@@ -1,5 +1,16 @@
 function download(version, platform) {
-    //const downloadLink = `path_to_downloads/version_${version}_${platform}bit.zip`;
+    // const downloadLink = `path_to_downloads/version_${version}_${platform}bit.zip`;
     const downloadLink = `https://github.com/reian-game/reian/raw/main/assets/sample.zip`;
-    window.location.href = downloadLink;
+
+    // Google Analytics にイベントを送信
+    //gtag('event', 'download', {
+    //   'event_category': 'Download',
+    //    'event_label': `Version ${version} - ${platform}bit`,
+    //    'value': 1
+    //});
+
+    // 少し待ってからダウンロードを実行
+    setTimeout(function () {
+        window.location.href = downloadLink;
+    }, 100); // 100ms待ってからリダイレクト
 }
