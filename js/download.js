c8699b7623312a57679e189cb0182d849664cb26
KEI0123 <keishu.sgtm@gmail.com>
2025-03-06 19:55:00 +0900

    ダウンロードされたときにGoogle Analyticsのイベントを送信する

--- js/download.js
+++ js/download.js
@@ -1,5 +1,14 @@
 function download(version, platform) {
-    //const downloadLink = `path_to_downloads/version_${version}_${platform}bit.zip`;
+    //　const downloadLink = `path_to_downloads/version_${version}_${platform}bit.zip`;
     const downloadLink = `https://github.com/reian-game/reian/raw/main/assets/sample.zip`;
+
+    // Google Analytics にイベントを送信
+    gtag('event', 'download', {
+        'event_category': 'Download',
+        'event_label': `Version ${version} - ${platform}bit`,
+        'value': 1
+    });
+
+    // ダウンロードを実行
     window.location.href = downloadLink;
 }
